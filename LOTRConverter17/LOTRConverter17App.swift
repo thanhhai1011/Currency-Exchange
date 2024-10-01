80e3347dab4b545c51a9a7905afa6413f5e873e9
hai.nguyen <hai.nguyen@dgvdigital.com>
2024-10-01 13:47:34 +0700

    Initial Commit

---
+++ LOTRConverter17/LOTRConverter17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverter17App.swift
+//  LOTRConverter17
+//
+//  Created by Hai Nguyen on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverter17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
